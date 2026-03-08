d82b25a632ce58dce6068cde8b0b99ca45b1e27f
nagaranikondaboina <nagaranikondaboina@gmail.com>
2026-03-08 14:02:43 +0530

    Create train_model.py

---
+++ train_model.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+from PIL import Image
+import os
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+path = 'dataset'
+
+faces = []
+ids = []
+
+for image in os.listdir(path):
+
+    img_path = os.path.join(path, image)
+
+    gray_img = Image.open(img_path).convert('L')
+    img_numpy = np.array(gray_img, 'uint8')
+
+    id = int(image.split(".")[1])
+
+    faces.append(img_numpy)
+    ids.append(id)
+
+recognizer.train(faces, np.array(ids))
+
+recognizer.save('trainer/trainer.yml')
+
+print("Model Trained Successfully")
